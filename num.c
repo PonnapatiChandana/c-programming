3ee11f80b009a7d1541a2f9276978ac70d677226
chandana <122780473+PonnapatiChandana@users.noreply.github.com>
2023-02-23 09:39:36 +0530

    Add files via upload

---
+++ num.c
@@ -0,0 +1,11 @@
+/*Display this AP-100,97,94,..upto all terms which are positive*/
+#include<stdio.h>
+int main(){
+    printf("The AP is as follows:");
+    for(int i=100;i>=1;i=i-3){
+        if(i>0){
+        printf("%d ",i);
+         
+    }}
+return 0;    
+}
