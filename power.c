80cc1fe74e71a5328c3c77fe50418b8d758a89bf
chandana <122780473+PonnapatiChandana@users.noreply.github.com>
2023-02-28 18:07:28 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,19 @@
+//Two numbers are entered through the keyboard.Write a program to find the value of one number raised to the power of another
+#include<stdio.h>
+int a;
+int b;
+int i;
+int main(){
+    printf("Enter the base:");
+    scanf("%d",&a);
+    printf("Enter the power:");
+    scanf("%d",&b);
+    int power=1;
+    for(i=1;i<=b;i++){
+        power=power*a;
+    }
+    printf("%d raised to %d  times is %d",a,b,power);
+return 0;
+}
+
+
