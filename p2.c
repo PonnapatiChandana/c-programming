dc893eac1766d9ef0db198e02d9ea83e8f384470
chandana <122780473+PonnapatiChandana@users.noreply.github.com>
2023-03-02 17:55:42 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,19 @@
+/*Print the given pattern
+* * * *
+* * * *
+* * * *
+* * * * */
+#include<stdio.h>
+int main(){
+   int n;
+   printf("Enter number of rows and column:");
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++){
+      for(int j=1;j<=n;j++){
+         printf("* ");
+      }
+      printf("\n");
+   }
+}
+
+
