b06929cddccae94a92d982b667a9aad9b51ee69f
chandana <122780473+PonnapatiChandana@users.noreply.github.com>
2023-03-02 18:04:00 +0530

    Add files via upload

---
+++ p7.c
@@ -0,0 +1,19 @@
+/*print the given pattern
+1 2 3
+1 2 
+1  
+*/
+#include<stdio.h>
+int main()
+{
+   int n;
+   printf("Enter the value:");
+   scanf("%d",&n);
+   for (int i=1;i<=n;i++){
+      for(int j=1;j<=n+1-i;j++){
+         printf("%d ",j);
+      }
+      printf("\n");
+   }
+   return 0;
+}
